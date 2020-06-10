caeac564a4c234609a34d82ba0229cc8a8306b5f
tanxinzheng <tanxinzheng@139.com>
2020-06-10 20:33:12 +0800

    Spring Cloud Gateway 文档聚合

--- boot-core/src/main/java/com/github/tanxinzheng/framework/model/BaseQuery.java
+++ boot-core/src/main/java/com/github/tanxinzheng/framework/model/BaseQuery.java
@@ -2,6 +2,9 @@ package com.github.tanxinzheng.framework.model;
 
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.github.tanxinzheng.framework.utils.MybatisPlusUtils;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
 import java.io.Serializable;
@@ -11,12 +14,18 @@ import java.util.List;
  * Created by tanxinzheng on 17/6/6.
  */
 @Data
+@ApiModel(value = "查询条件基类")
 public class BaseQuery implements Serializable {
 
     private BaseQueryCondition query;
+
+    @ApiModelProperty(value = "关键词")
     private String keyword;
+    @ApiModelProperty(value = "每页条数")
     private Long pageSize;
+    @ApiModelProperty(value = "当前页码")
     private Long pageNum;
+
     private List<QueryWrapperCondition> conditions;
 
     public QueryWrapper getQueryWrapper(){
